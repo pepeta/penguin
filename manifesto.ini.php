8c58c6b16f8cde06d7bcaaadb35d88d112253e8b
pepeta <pepeta0307@gmail.com>
2015-10-16 02:29:47 +0900

    penguinテーマの修正作業開始

---
+++ manifesto.ini.php
@@ -0,0 +1,20 @@
+<?php
+/**
+
+[Manifesto]
+Name="Tropical Penguin"
+Depends=Legacy_RenderSystem,legacy
+Url="http://ryus.co.jp/"
+Version="1.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=RYUS.INC
+ScreenShot="screenshot.gif"
+Description="Tropical Penguin"
+
+License="GPL"
+
+*/
+?>
